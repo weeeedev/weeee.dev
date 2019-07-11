f91fe498cc42929f773c1031d856b49214b76578
Ville-Veikko Kovalainen <weeee@iki.fi>
2019-07-11 21:43:44 +0300

    Add travis config

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eEuo pipefail
+
+function cleanup {
+    rm -rf deployment/
+}
+
+trap cleanup EXIT
+
+echo "Deploying to GitHub pages"
+
+git clone -b master git@github.com:weeeedev/weeeedev.github.io.git deployment
+rsync -av --delete --exclude ".git" public/ deployment
+
+cd deployment
+git add --all
+git commit -m "Built on ${date}, commit ${TRAVIS_COMMIT} by job ${TRAVIS_JOB_NUMBER}" || true
+git push
